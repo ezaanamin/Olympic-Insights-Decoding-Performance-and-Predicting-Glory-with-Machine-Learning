6ddc2c0991047f4e404a129765b85be9bddfbc40
ezaanamin <ezaan.amin@gmail.com>
2024-11-21 22:29:18 +0500

    medal tally

--- web/app.py
+++ web/app.py
@@ -7,6 +7,7 @@ region_df=pd.read_csv("/home/ezaan-amin/Documents/PortFolio/Olympic Insights_Dec
 
 
 df=preprocessing.preprocess(df,region_df)
+st.sidebar.title("Olympics Analysis ")
 user_menu=st.sidebar.radio(
     'Select an Option',
     ("Medal Tally",'Overall Analysis','Country wise Analysis','Athlete wise Analysis')
@@ -15,5 +16,9 @@ user_menu=st.sidebar.radio(
 
 
 if user_menu=="Medal Tally":
+    st.header("Medal Tally")
+    year,country=helper.country_year_list(df)
+    selected_year=st.sidebar.selectbox("Select years",year)
+    selected_year=st.sidebar.selectbox("Select country",country)
     medal_tally=helper.metal_tally(df)
     st.dataframe(medal_tally)
--- web/helper.py
+++ web/helper.py
@@ -1,6 +1,18 @@
+import numpy as np
 def metal_tally(df):
     medal_tally=df.drop_duplicates(subset=['Team','NOC','City','Year','Medal','Games','Event','Sport'])
     m_tally=medal_tally.groupby("region").sum()[['Bronze', 'Gold', 'Silver']].sort_values("Gold",ascending=False).reset_index()
     m_tally['total']=m_tally['Gold']+m_tally['Bronze']+m_tally['Silver']
     return m_tally
+
+
+def country_year_list(df):
+    year=df['Year'].unique().tolist()
+    year.sort()
+    year.insert(0,'Overall')
+    country=np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0,'Overall')
+    return country,year
+
 
